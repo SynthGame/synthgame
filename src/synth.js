7d918c4498af99efb1f075474fb0f664b1487630
NickolasBoyer <rut.willems@gmail.com>
2018-08-22 12:41:06 +0800

    add debug messages and other methods

--- src/synth.js
+++ src/synth.js
@@ -1,5 +1,13 @@
 import Tone from 'tone';
 
+// define logging mechanism
+const log = console.info
+
+// let allows aditing object, const not??
+export let state = {
+  synthToneLength: '4n'
+}
+
 // returns configured sequence 
 export const createLoop = ({
   noteArray,
@@ -11,14 +19,22 @@ export const createLoop = ({
 export const synth = new Tone.Synth().toMaster()
 
 export const playNote = (note) => {
-  return synth.triggerAttackRelease(note, "4n")
+  log(`Playing note: ${note}`)
+  return synth.triggerAttackRelease(note, state.synthToneLength)
 }
 
 export const setBpm = (bpm) => {
+  log(`setting BPM length to: ${bpm}`)
   return Tone.Transport.bpm.value = bpm;
 }
 
+export const setSynthToneLength = (length) => {
+  log(`setting tone length to: ${length}`)
+  return state.synthToneLength = length
+}
+
 export const start = () => {
+  log(`starting Tone.js Transport`)
   return Tone.Transport.start();
 }
 
