5cb5897e826f1341cb577db955bd502820d0a61d
Jakub Strebeyko <strebeyko@gmx.com>
2018-08-27 09:02:37 +0200

    enhance display module

--- src/components/display.vue
+++ src/components/display.vue
@@ -1,9 +1,9 @@
 <template>
-  <svg v-if="module=='filter'" fill="rgb(14, 80, 186)"
-  style="margin: auto;">
-  <rect :width="displayWidth" :height="displayHeight"/>
+  <!-- envelope display -->
+  <svg v-if="module=='filter'"
+  style="margin: auto; width: 100%">
+  <rect :width="displayWidth" :height="displayHeight" fill="rgb(14, 80, 186)"/>
     <path stroke="black" stroke-width="3" :d="filterPath" fill="black" style="fill-rule: nozero"/>
-
 	  <text x="45%" y="40%" fill="blue">
       <tspan>L: {{lowpass}}</tspan>
       <tspan x="45%" y="50%">H: {{highpass}}</tspan>
@@ -11,6 +11,12 @@
     </text>
 
   </svg>
+  <!-- envelope display -->
+    <svg v-else-if="module=='envelope'"
+  style="margin: auto;">
+  <rect  :width="displayWidth" :height="displayHeight" fill="rgb(244, 200, 66)"/>
+
+    </svg>
 </template>
 
 <script>
@@ -21,6 +27,16 @@
 // [v]. turn the curve from the cubic one to quadratic
 // [v]. make the lowpass & highpass one method
 // [v]. do something about the colors
+
+
+// [] REDISiGN in accrodance with knobs !!
+// embed into modules
+
+// type:
+//
+//
+//
+
 // ===============
 // [] the required margin data object and path
 // [] have the other knobs connected to the filter audio output
@@ -49,7 +65,8 @@ export default {
     return {
       // default values to not have the calculation derail at created()
       displayHeight: 300,
-      displayWidth: 600
+      displayWidth: 600,
+      curveAmnt: 90
     }
   },
   mounted () {
@@ -60,31 +77,33 @@ export default {
   },
   methods: {
     // curve drawing helper:
-    curve(dir, amnt) {
+    curve(dir) {
       var mod1 = (dir === "left-up" || dir === "left-down") ? 1 : 0
       var mod2 = (dir === "left-up" || dir === 'left-down') ? 0 : (dir === "down-right") ? -1: 1
       var mod3 = 1
       var mod4 = (dir === "left-down" || dir === "up-right") ? 1 : -1
 
-      return " q "+(amnt*mod1) + " " + (amnt*mod2) + " " + (amnt*mod3) + " " + (amnt*mod4)
+      return " q "+(this.curveAmnt*mod1) + " " + (this.curveAmnt*mod2) + " " + (this.curveAmnt*mod3) + " " + (this.curveAmnt*mod4)
     }
   },
   computed: {
     filterPath() {
       // TODO: hardcoded = change it
-      let lowpassVar = ((this.lowpass/(10000-50))*250)-3
-      let highpassVar = ((this.highpass/(10000-50))*250)-30
+      let lowpassVar = (this.lowpass/(10000-50))*(((this.displayWidth/2)-4*this.curveAmnt))
+      let highpassVar = ((this.highpass/(10000-50))*((this.displayWidth/2)-4*this.curveAmnt))
       let gainVar = ((this.gain/(10000-50))*(this.displayHeight-110))
 
-      let line = "M-300,"+(this.displayHeight)+
-                " h 270 " + lowpassVar +
-                 this.curve("left-up", 50) +
+      let line = "M0,"+(this.displayHeight)+
+                " h "+ lowpassVar+
+                 this.curve("left-up") +
                 " v -"+(gainVar) + " " +
-                 this.curve("down-right", 50)+
-                " h "+((this.displayWidth/2)-lowpassVar-80)+" h "+((this.displayWidth/2)-highpassVar-80)+
-                 this.curve("left-down", 50) +
+                 this.curve("down-right")+
+                // " h "+((this.displayWidth/2)-lowpassVar-80)+" h "+((this.displayWidth/2)-highpassVar-80)+
+                //  " h " + ((((this.displayWidth-4*this.curveAmnt)/2) -lowpassVar) + (((this.displayWidth-4*this.curveAmnt)/2) - highpassVar)) +
+                // " h " + ((-(highpassVar+lowpassVar))>=0 ? (-(highpassVar+lowpassVar)) : 0) +
+                 this.curve("left-down") +
                 " v "+ (gainVar) + " " +
-                 this.curve("up-right", 50) +
+                 this.curve("up-right") +
                 " h 300"+ highpassVar+ "Z"
       return line
     }
