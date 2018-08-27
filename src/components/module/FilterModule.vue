4a467964ec1a9af05577d2d2d141ac7195085156
NickolasBoyer <rut.willems@gmail.com>
2018-08-27 09:59:01 +0800

    remove highscores from component

--- src/components/module/FilterModule.vue
+++ src/components/module/FilterModule.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="module">
     <display  style="margin-left:10%;background: #141414;height:15em;width:80%" module="filter" :lowpass="cutOffFreq" :highpass="cutOffFreq1" :gain="gain"/>
-          <circle-slider
-            v-model="cutOffFreq"
-            :min="0"
-            :max="10000"
-            knobColor="#6e01d1"
-          ></circle-slider>
-          <circle-slider
-            v-model="cutOffFreq1"
-            :min="50"
-            :max="10000"
-            knobColor="#6e01d1"
-          ></circle-slider>
-    bandpass: <circle-slider
-            v-model="gain"
-            :min="50"
-            :max="10000"
-            knobColor="#6e01d1"
-          ></circle-slider>
-    <ul>
-      <div v-for="score in highscores" :key="score.id">
-        <strong>{{`🏆: ${score.name}: ${score.score}`}}</strong>
-      </div>
-    </ul>
+      <circle-slider
+        v-model="cutOffFreq"
+        :min="0"
+        :max="10000"
+        knobColor="#6e01d1"
+      ></circle-slider>
+      <circle-slider
+        v-model="cutOffFreq1"
+        :min="50"
+        :max="10000"
+        knobColor="#6e01d1"
+      ></circle-slider>
+      <span>bandpass: </span>
+      <circle-slider
+        v-model="gain"
+        :min="50"
+        :max="10000"
+        knobColor="#6e01d1"
+      ></circle-slider>
   </div>
 </template>
 
