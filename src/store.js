82e0e5eed12dbdc1402839e0ce2c5ed53ae608b2
NickolasBoyer <rut.willems@gmail.com>
2018-08-30 12:10:00 +0800

    add goal reached check

--- src/store.js
+++ src/store.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import mapValues from 'lodash/mapValues'
 import random from 'lodash/random'
-import matches from 'lodash/matches'
+import values from 'lodash/values'
+import flatMap from 'lodash/flatMap'
+import inRange from 'lodash/inRange'
 
 Vue.use(Vuex)
 
@@ -11,7 +13,7 @@ export default new Vuex.Store({
     audioParameters: {
       delay: {
         delayTime: 20,
-        wet: 0.2,
+        wet: 0,
         feedback: 10
       },
       envelope: {
@@ -38,28 +40,29 @@ export default new Vuex.Store({
         phase: 0
       },
       reverb: {
-        roomSize: 0.5,
-        wet: 0.2
+        roomSize: 0,
+        wet: 0
       }
     },
     gameState: {
+      margin: 10,
       goal: {
         delay: {
-          delayTime: 40,
-          wet: 0.2,
+          delayTime: 20,
+          wet: 0,
           feedback: 10
         },
         envelope: {
           attack: 11,
-          decay: 21,
-          sustain: 9,
-          release: 12
+          decay: 60,
+          sustain: 52,
+          release: 34
         },
         filter: {
-          cutOffFreq: 12000,
+          cutOffFreq: 50,
           type: 0,
           setQ: 50,
-          gain: 1
+          gain: 50
         },
         lfo: {
           frequency: 0,
@@ -68,13 +71,13 @@ export default new Vuex.Store({
         },
         oscillator: {
           frequency: 2,
-          typeOsc: 1,
-          detune: 60,
+          typeOsc: 2,
+          detune: 40,
           phase: 0
         },
         reverb: {
-          roomSize: 0.5,
-          wet: 0.2
+          roomSize: 0,
+          wet: 0
         }
       }
     }
@@ -89,14 +92,33 @@ export default new Vuex.Store({
         ...state.audioParameters,
         ...preset
       }
+    },
+    setGoalToPreset (state, {preset}) {
+      // overwrite parameters from audiostate, this will not fill in nested objects
+      state.gameState.goal = {
+        ...state.gameState.goal,
+        ...preset
+      }
+    },
+    setMargin (state, {newMargin}) {
+      // overwrite parameters from audiostate, this will not fill in nested objects
+      state.gameState.margin = newMargin
     }
   },
   getters: {
-    gameGoalMatcher: (state, getters) => {
-      return matches(state.gameState.goal)
+    allParametersMatchGoal: (state, getters) => {
+      return flatMap(getters.audioParametersMatchGoalWithMargin, val => values(val))
+        .every(val => val)
     },
-    audioParametersMatchGoal: (state, getters) => {
-      return getters.gameGoalMatcher(state.audioParameters)
+    audioParametersMatchGoalWithMargin: (state) => {
+      return mapValues(state.audioParameters, (val, moduleName) => {
+        return mapValues(val, (val, parameterName) => {
+          return inRange(val, 
+            (state.gameState.goal[moduleName][parameterName] - state.gameState.margin), 
+            (state.gameState.goal[moduleName][parameterName] + state.gameState.margin)
+          )
+        })
+      })
     }
   },
   actions: {
@@ -106,6 +128,15 @@ export default new Vuex.Store({
         return random(0, 100)
       })
       return commit('setAudioParameterToPreset', {
+        preset: randomizeValues(state.audioParameters)
+      })
+    },
+    randomizGoalParameters ({state, commit}) {
+      const randomizeValues = obj => mapValues(obj, val => {
+        if(typeof val === 'object') return randomizeValues(val) // recuuursion whooo
+        return random(0, 100)
+      })
+      return commit('setGoalToPreset', {
         preset: randomizeValues(state.audioParameters)
       })
     }
