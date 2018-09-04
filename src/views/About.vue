6dd75c2204701a67e68685e18202f6d19c46d4ff
Jakub Strebeyko <strebeyko@gmx.com>
2018-09-04 10:24:11 +0200

    simplify the wave shape buttons

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <oscillator-module/>
   </div>
 </template>
+
+<script>
+import OscillatorModule from '@/components/module/OscillatorModule'
+export default {
+  components: {
+    OscillatorModule
+  }
+}
+</script>
+
+<style>
+
+</style>
