7185db6215b81856e220e43321bf3534cd51e76a
Jakub Strebeyko <strebeyko@gmx.com>
2018-09-05 17:09:35 +0200

    add enter key event listeners on mount (and remove on beforeDestroy) so the screens can be skipped

--- src/components/SuccessOverlay.vue
+++ src/components/SuccessOverlay.vue
@@ -15,12 +15,10 @@
               </g>
           </g>
       </svg>
-    <svg version="1.1"
+    <svg version="1.1" :style="computedRackedStyles"
      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
      x="0px" y="0px" width="205px" height="104px" viewBox="0 0 203 98" style="enable-background:new 0 0 203 98;"
      xml:space="preserve">
-    <defs>
-    </defs>
     <path id="loading_page_1_" class="st0" d="M10.9,90.9L0,77.2l0.5-15l10.3,13.7c0,0,6-1.1,22.6-6.9c14.8-5.5,33.4-6.6,34.3-6.6
         c5.9,0.2,9.2,2.5,14.6,6.3c4.2,2.9,9.5,6.6,17.7,10.5c0.1,0,0.1,0,0.2,0.1c0.4,0.2,0.7,0.4,1.1,0.6c18.9,8.9,46.7,5.9,57.8,4.2
         c30.1-4.6,47.6-26,40.6-49.7c-3.6-12.2-12.8-22.2-25.9-28.4c-11.7-5.5-25.3-7.3-39.2-5.1c-11.9,1.8-40.5,7.7-51.9,22.6l-0.1,0.1
@@ -36,6 +34,8 @@
 
 <script>
 import { keyframes, easing } from 'popmotion'
+import posed from 'vue-pose';
+
 export default {
   name: "Overlay",
   props: {
@@ -49,17 +49,30 @@ export default {
       ballHeight: 0
     }
   },
+  mounted() {
+    window.addEventListener('keyup', this.emitOnKey)
+  },
+  beforeDestroy() {
+    window.removeEventListener('keyup', this.emitOnKey)
+  },
+  methods: {
+    emitOnKey() {
+      if (event.keyCode === 13) {
+        this.$emit('next');
+      }
+    }
+  },
   created() {
-    // keyframes({
-    //   values: [ // you can include anything here, jus values or '10px' or colors whatever
-    //     { x: 0, background: 'hsla(125, 100, 50, 1)' },
-    //     { x: -60, background: 'hsla(20, 100, 60, 1)' },
-    //     { x: 0, background: 'hsla(20, 100, 60, 1)' }
-    //   ],
-    //   loop: Infinity,
-    //   duration: 1000,
-    //   easings: [easing.easeOut, easing.easeIn, easing.lineair]
-    // }).start(v => this.ballHeight = `translateY(${v.x + 20}px)`)
+    keyframes({
+      values: [ // you can include anything here, jus values or '10px' or colors whatever
+        { x: 0, background: 'hsla(125, 100, 50, 1)' },
+        { x: -60, background: 'hsla(20, 100, 60, 1)' },
+        { x: 0, background: 'hsla(20, 100, 60, 1)' }
+      ],
+      loop: Infinity,
+      duration: 1000,
+      easings: [easing.easeOut, easing.easeIn, easing.lineair]
+    }).start(v => this.ballHeight = `translateY(${v.x + 20}px)`)
   },
   computed: {
     gameScore () {
@@ -70,6 +83,15 @@ export default {
     },
     gameLevel () {
       return this.$store.state.gameState.level
+    },
+    computedRackedStyles() {
+      return {'animation-name': 'racket-movement',
+                'animation-duration': '1s',
+                 'animation-iteration-count': '10',
+                  'animation-direction': 'alternate', /* or: normal */
+                  'animation-timing-function': 'ease-out', /* or: ease, ease-in, ease-in-out, linear, cubic-bezier(x1, y1, x2, y2) */
+                  'animation-fill-mode': 'forwards', /* or: backwards, both, none */
+                  'animation-delay': '2s' /* or: Xms */}
     }
   }
 }
@@ -132,4 +154,13 @@ export default {
     padding: 1rem 2rem;
   border: 1px solid white;
 }
+
+// ok, so apparently regular keyframes do not work unless defined somewhere higher:
+// https://stackoverflow.com/questions/47283989/why-are-css-keyframe-animations-broken-in-vue-components-with-scoped-styling
+// @keyframes racket-movement {
+//   0% { transform: rotate(0) }
+//   20% { transform: rotate(10deg)}
+//   50% { transform: rotate(20deg) }
+//   100% { transform: rotate(100%) }
+// }
 </style>
