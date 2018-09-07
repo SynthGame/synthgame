e5a6522b217441ab1fd6713e59581c851be9d868
Jakub Strebeyko <strebeyko@gmx.com>
2018-09-07 07:45:16 +0200

    add ping-pong animation to be displayed after successfully passing a level

--- src/components/SuccessOverlay.vue
+++ src/components/SuccessOverlay.vue
@@ -7,25 +7,26 @@
         <h2>High Score: {{gameHighScore}}</h2>
         <!-- <p class="score">Your current score: {{gameScore}}</p> -->
       </div>
-      <svg :style="{'transform': ballHeight}" width="37px" height="37px" viewBox="0 0 37 37" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <defs></defs>
-          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g id="loadingscreen_icon1-01" transform="translate(-120.000000, 0.000000)" stroke="#FFFFFF" fill-rule="nonzero" stroke-width="1.5819" fill="#FF8575">
-                  <circle id="Oval" cx="138.6" cy="18.2" r="17.3"></circle>
-              </g>
-          </g>
-      </svg>
-    <svg version="1.1" :style="computedRackedStyles"
-     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
-     x="0px" y="0px" width="205px" height="104px" viewBox="0 0 203 98" style="enable-background:new 0 0 203 98;"
-     xml:space="preserve">
-    <path id="loading_page_1_" class="st0" d="M10.9,90.9L0,77.2l0.5-15l10.3,13.7c0,0,6-1.1,22.6-6.9c14.8-5.5,33.4-6.6,34.3-6.6
-        c5.9,0.2,9.2,2.5,14.6,6.3c4.2,2.9,9.5,6.6,17.7,10.5c0.1,0,0.1,0,0.2,0.1c0.4,0.2,0.7,0.4,1.1,0.6c18.9,8.9,46.7,5.9,57.8,4.2
-        c30.1-4.6,47.6-26,40.6-49.7c-3.6-12.2-12.8-22.2-25.9-28.4c-11.7-5.5-25.3-7.3-39.2-5.1c-11.9,1.8-40.5,7.7-51.9,22.6l-0.1,0.1
-        c-5.4,6-8,10.7-10.1,14.4c-2.7,4.8-4.1,7.4-9.3,9.4 M78.5,28.5l23.1,52v10.7 M0.5,62.2c25.1-7.5,38.1-4,62.8-14.9l9.4,6.1l-7.4,9.1
-        v11.2 M10.9,75.9v15c0,0,40.5-18.4,55.8-17.3s33.7,28.9,83.7,21.5s50.9-49.3,50.9-49.3"/>
+      <svg viewBox="0 0 250 250" height="400px" width="400px">
+        <g :transform="`rotate(${anim.ping.racketTurn}) translate(0, ${anim.ping.racketYOffset})`">
+          <svg version="1.1"
+          xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+          x="40px" y="100px" width="350px" height="127px"
+          xml:space="preserve">
+            <path id="loading_page_1_" class="st0" d="M10.9,90.9L0,77.2l0.5-15l10.3,13.7c0,0,6-1.1,22.6-6.9c14.8-5.5,33.4-6.6,34.3-6.6
+                c5.9,0.2,9.2,2.5,14.6,6.3c4.2,2.9,9.5,6.6,17.7,10.5c0.1,0,0.1,0,0.2,0.1c0.4,0.2,0.7,0.4,1.1,0.6c18.9,8.9,46.7,5.9,57.8,4.2
+                c30.1-4.6,47.6-26,40.6-49.7c-3.6-12.2-12.8-22.2-25.9-28.4c-11.7-5.5-25.3-7.3-39.2-5.1c-11.9,1.8-40.5,7.7-51.9,22.6l-0.1,0.1
+                c-5.4,6-8,10.7-10.1,14.4c-2.7,4.8-4.1,7.4-9.3,9.4 M78.5,28.5l23.1,52v10.7 M0.5,62.2c25.1-7.5,38.1-4,62.8-14.9l9.4,6.1l-7.4,9.1
+                v11.2 M10.9,75.9v15c0,0,40.5-18.4,55.8-17.3s33.7,28.9,83.7,21.5s50.9-49.3,50.9-49.3"/>
 
-    </svg>
+          </svg>
+        </g>
+        <svg  width="37px" height="390px" x="145px" y="-35px" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g id="loadingscreen_icon1-01" :transform="`translate(-120.000000, ${anim.ping.ballHeight})`" stroke="#FFFFFF" fill-rule="nonzero" stroke-width="1.5819" :fill="currentColor">
+                    <circle id="Oval" cx="138.6" cy="18.2" r="17.3"></circle>
+                </g>
+        </svg>
+      </svg>
     <!-- <p>Don't stop here!</p> -->
     <button class="button-next" @click="$emit('next')">NEXT LEVEL</button>
     </div>
@@ -34,6 +35,8 @@
 
 <script>
 import { keyframes, easing } from 'popmotion'
+import { MODULE_OSCILLATOR_COLOR, MODULE_ENVELOPE_COLOR, MODULE_FILTER_COLOR, MODULE_LFO_COLOR, MODULE_DELAY_COLOR, MODULE_REVERB_COLOR } from '@/constants'
+import { color } from 'style-value-types';
 
 export default {
   name: "Overlay",
@@ -45,11 +48,28 @@ export default {
   },
   data () {
     return {
-      ballHeight: 0
+      currentAnim: null,
+      anim: {
+        ping: {
+          ballHeight: 0,
+          racketTurn: 0,
+          racketYOffset: 0
+        },
+        drum: {}
+      },
+      colorArray: [],
+      currentColor: ''
     }
   },
+  created() {
+    },
   mounted() {
+    // this.anim[Math.floor(Math.random()*this.anim.length)]
+    console.log(this.anim[drum])
+    this.currentAnim = Math.floor(Math.random()*this.anim.length)
     window.addEventListener('keyup', this.emitOnKey)
+    this.colorArray.push(MODULE_OSCILLATOR_COLOR, MODULE_ENVELOPE_COLOR, MODULE_FILTER_COLOR, MODULE_LFO_COLOR, MODULE_DELAY_COLOR, MODULE_REVERB_COLOR)
+    this.changeColor()
   },
   beforeDestroy() {
     window.removeEventListener('keyup', this.emitOnKey)
@@ -64,14 +84,32 @@ export default {
   created() {
     keyframes({
       values: [ // you can include anything here, jus values or '10px' or colors whatever
-        { x: 0, background: 'hsla(125, 100, 50, 1)' },
-        { x: -60, background: 'hsla(20, 100, 60, 1)' },
-        { x: 0, background: 'hsla(20, 100, 60, 1)' }
+        { ballY: 0, racketRotate: 0, racketYOffset: 0},
+        { ballY: -180, racketRotate: 12, racketYOffset: 20},
+        { ballY: 0, racketRotate:0 , racketYOffset: 0}
       ],
       loop: Infinity,
       duration: 545,
       easings: [easing.easeOut, easing.easeIn, easing.lineair]
-    }).start(v => this.ballHeight = `translateY(${v.x + 20}px)`)
+    }).start({
+        update: v => {
+          this.anim.ping.ballHeight = v.ballY
+          this.anim.ping.racketTurn = v.racketRotate
+          this.anim.ping.racketYOffset = v.racketYOffset
+          if (v.ballY == 0) {
+            this.changeColor()
+          }
+        },
+        complete: () => {console.log('lalala')}
+      })
+  },
+  methods:{
+    changeColor() {
+      //make sure they do not repeat:
+      // let list = this.colorArray.splice(this.colorArray.indexOf(this.currentColor), 1)
+      // do so they don't repeat list.splice( list.indexOf('foo'), 1 );
+      this.currentColor = this.colorArray[Math.floor(Math.random()*this.colorArray.length)]
+    }
   },
   computed: {
     gameScore () {
@@ -91,7 +129,8 @@ export default {
                   'animation-timing-function': 'ease-out', /* or: ease, ease-in, ease-in-out, linear, cubic-bezier(x1, y1, x2, y2) */
                   'animation-fill-mode': 'forwards', /* or: backwards, both, none */
                   'animation-delay': '2s' /* or: Xms */}
-    }
+    },
+
   }
 }
 </script>
