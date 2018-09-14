5bfea9d3d7e1604411d0afa8982a5466709bfb96
Jakub Strebeyko <strebeyko@gmx.com>
2018-09-14 11:10:55 +0200

    add copy button and some styling

--- src/components/AfterCreateOverlay.vue
+++ src/components/AfterCreateOverlay.vue
@@ -2,8 +2,10 @@
   <div class="overlay">
     <div class="overlay-content-wrapper">
         <h2>Copy this link and paste it on RedBull website to have a chance to win a flight and entry ticket for MASS!</h2>
-        <span>{{link}}</span>
+        <span class="link">{{link}}</span>
       <div>
+        <button class="button-next" @click="copy">{{copyButtonText}}</button>
+
         <button class="button-next" @click="$emit('closeCreate')">return</button>
       </div>
     </div>
@@ -20,6 +22,11 @@ export default {
       default: 'a link'
     }
   },
+  data() {
+    return {
+      copyButtonText: 'copy'
+    }
+  },
   mounted() {
     window.addEventListener('keydown', this.emitOnKey)
   },
@@ -31,6 +38,15 @@ export default {
       if (event.keyCode === 13) {
         this.$emit('closeCreate');
       }
+    },
+    copy() {
+      const el = document.createElement('textarea');
+      el.value = this.link;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      this.copyButtonText="copied!"
     }
   },
   computed: {
@@ -40,4 +56,11 @@ export default {
 
 <style scoped lang="scss">
 
+.link {
+  text-transform: none;
+  background-color:  #fff;
+  color: #000;
+  font-size: 2rem;
+  padding: 0.5rem;
+}
 </style>
