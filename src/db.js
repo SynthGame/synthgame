d98f5218eb996589fe01f74005a4f5f000fc02ff
Lawson Taylor <lawson.taylor@protonmail.com>
2019-02-21 00:40:49 +0800

    game room creation and update functions

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore' // https://cloud.google.com/nodejs/docs/reference/firestore/
+import * as crypto from 'crypto';
 
 // Initialize Firebase
 const config = {
@@ -15,14 +16,58 @@ firebase.initializeApp(config)
 
 export const db = firebase.firestore()
 
-db.settings({timestampsInSnapshots: true}) // time snapshot will change in future version
+db.settings({ timestampsInSnapshots: true }) // time snapshot will change in future version
 
 const scoreRef = db.collection('highscores')
 const presetRef = db.collection('customPresets')
 
+const gameRoomRefs = db.collection('gameRooms');
+
+// CREATE A ROOM WITH INITAL HIGHSCORE - return URL.
+export const createNewRoom = ({ name, score }) => {
+  const URL = crypto.randomBytes(12).toString('hex');
+  return gameRoomRefs.doc(URL).set({
+    game: [{
+      name,
+      score,
+    }]
+  }).then(() => {
+    return URL;
+  })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+}
+
+// ADD A PLAYERS SCORE TO AN EXISTING GAME
+export const updateRoom = ({ url, name, score }) => {
+  return gameRoomRefs.doc(url).update({
+    game: firebase.firestore.FieldValue.arrayUnion({
+      name,
+      score
+    })
+  });
+}
+
+// RETURN GAME WITH HIGHSCORE DATA
+export const getRoom = (url) => {
+  return gameRoomRefs.doc(url).get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        return doc.data();
+      } else {
+        console.log("No such document!");
+        return { error: 'No Game exists!' }
+      }
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+}
+
 // Highscore funtions
 
-export const addHighscore = ({name, score}) => {
+export const addHighscore = ({ name, score }) => {
   return scoreRef.add({
     name,
     score
@@ -59,7 +104,7 @@ export const getHighscoresByName = (name) => {
  *  @param  {Object} obj.sequenceArray - Array with 16 objects that define the steps in the sequence
  *  @return {Promise} Returns the uid of the newly created preset
  */
-export const addPreset = ({name, bpm, parameterValues, sequenceArray}) => {
+export const addPreset = ({ name, bpm, parameterValues, sequenceArray }) => {
   return presetRef
     .add({
       name,
