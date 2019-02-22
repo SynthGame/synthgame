ae2cc295d730bdb80581279b23a9d80e9cb6f94c
Momcilo Popov <popovmomcilo@gmail.com>
2019-02-22 10:51:05 +0800

    Remove extra code

--- src/components/module/SequencerModule/SequencerButton.vue
+++ src/components/module/SequencerModule/SequencerButton.vue
@@ -1,21 +1,8 @@
 <template>
-  <!-- button with mouseover listener -->
-  <!-- <button
-  @mousedown="$emit('click', $event)"
-  @mouseover="toggleOnOff($event)"
-  class="sequencer-button">
-    <div class="sequencer-button__content">
-      <div :class="{
-        'sequencer-button__indicator': true,
-        'sequencer-button__indicator--active': buttonActive,
-        'sequencer-button__indicator--selected': buttonSelected
-      }"></div>
-    </div>
-  </button> -->
   <button
-  @keydown.enter="$emit('click', $event)"
-  @mousedown="$emit('click', $event)"
-  class="sequencer-button">
+    @keydown.enter="$emit('click', $event)"
+    @mousedown="$emit('click', $event)"
+    class="sequencer-button">
     <div :class="{
       'sequencer-button__indicator': true,
       'sequencer-button__indicator--active': buttonActive,
@@ -23,16 +10,7 @@
     }" />
   </button>
 </template>
-
 <script>
-// let mouseIsDown = 0;
-// document.body.onmousedown = function() {
-//   mouseIsDown = mouseIsDown + 1;
-// }
-// document.body.onmouseup = function() {
-//   mouseIsDown = mouseIsDown - 1;
-// }
-
 export default {
   name: 'SequencerButton',
   props: {
